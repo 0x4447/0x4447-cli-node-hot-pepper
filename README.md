f70c704444d6447df6f3fddb4e749a5fb5060b66
David Gatti <github@gatti.pl>
2017-07-22 14:52:04 +0200

    Init

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # toSystemD
-😍 A SystemD config generators for NodeJS projects
+
+If you work with NodeJS projects and prefer to run them on a Linux box using SystemD as the process manager. Then this small project will help you save time by creating the .service file for you based on the content of the `package.json` file and the location in which the CLI was executed.
+
+# How to run
+
+Run the `tosystemd` command in the directory where you project is located, and in the end you'll get a new file with the correct SystemD configuration.
+
+This App will also output all the commands that you need to run to make the service working.
+
+
+# The End
+
+If you've enjoyed this article/project, please consider giving it a 🌟 or donate.
+
+- [![Donate](https://img.shields.io/badge/Donate-PayPal-green.svg)](https://www.paypal.me/gattidavid/25)
+- [![Star on GitHub](https://img.shields.io/github/stars/davidgatti/toSystemD.svg?style=social)](https://github.com/davidgatti/toSystemD/stargazers)
+- [![Watch on GitHub](https://img.shields.io/github/watchers/davidgatti/toSystemD.svg?style=social)](https://github.com/davidgatti/toSystemD/watchers)
+
+Also check out my [GitHub account](https://github.com/davidgatti), where I have other articles and apps that you might find interesting.
+
+---
+
+## Where to follow
+
+You can follow me on social media 🐙😇, at the following locations:
+
+- [GitHub](https://github.com/davidgatti)
+- [Twitter](https://twitter.com/dawidgatti)
+- [Instagram](https://www.instagram.com/gattidavid/)
+
+## More about me
+
+I don’t only live on GitHub, I try to do many things not to get bored 🙃. To learn more about me, you can visit the following links:
+
+- [Podcasts](http://david.gatti.pl/podcasts)
+- [Articles](http://david.gatti.pl/articles)
+- [Technical Articles](http://david.gatti.pl/technical_articles)
+- [Software Projects](http://david.gatti.pl/software_projects)
+- [Hardware Projects](http://david.gatti.pl/hardware_projects)
---
+++ index.js
@@ -0,0 +1,120 @@
+#!/usr/bin/env node
+
+let fs = require('fs');
+let os = require('os');
+
+//
+//	Check if the SystemD directory exists
+//
+let is_systemd = fs.existsSync("/etc/systemd/system");
+
+//
+//	Warn the user that the directory is not present
+//
+if(!is_systemd)
+{
+	return console.log("SystemD directory not found")
+}
+
+//
+//	Open the app.json file.
+//
+fs.readFile('package.json', 'utf8', function(err, data) {
+
+	//
+	//	1.	Display Error if any
+	//
+	err && console.log(err.message)
+
+	//
+	//	2.	Convert the content of the file in to a JS Object
+	//
+	let parsed = JSON.parse(data);
+
+	//
+	//	3.	Get all the necessary data
+	//
+	let description = parsed.name;
+	let documentation = parsed.repository.url;
+	let cwd = process.cwd();
+	let user = os.userInfo().username;
+
+	//
+	//	4.	An array where I'm going to store the whole file before saving it
+	//
+	let file = [];
+
+	//
+	//	5.	Add data to the array, which in the end will be used to create
+	//		the .service file
+	//
+	file.push("[Unit]");
+	file.push("Description=" + description);
+	file.push("Documentation=" + documentation);
+	file.push("After=network.target");
+
+	file.push("");
+
+	file.push("[Service]");
+	file.push("EnvironmentFile=" + cwd + "/.emv");
+	file.push("Type=simple");
+	file.push("User=" + user);
+	file.push("Group=" + user);
+	file.push("WorkingDirectory=" + cwd);
+	file.push("ExecStart=/usr/bin/node workers/server");
+	file.push("StandardOutput=syslog");
+	file.push("StandardError=syslog");
+	file.push("SyslogIdentifier=" + parsed.name);
+	file.push("Restart=on-failure");
+	file.push("RestartSec=3");
+	file.push("KillMode=process");
+	file.push("ExecReload=/bin/kill -HUP $MAINPID");
+
+	file.push("");
+
+	file.push("[Install]");
+	file.push("WantedBy=multi-user.target");
+
+	//
+	//	Join each element of the array in to one big file where each element
+	//	is in its own line
+	//
+	let service_file = file.join("\n");
+
+	//
+	//	6.	Save the data in to the .env file.
+	//
+	fs.writeFile(parsed.name + ".service", service_file, (error) => {
+
+		//
+		//	1.	Display Error if any
+		//
+		if(error)
+		{
+			console.log(error.message);
+		}
+
+		console.log("");
+
+		//
+		//	Show the user what happened
+		//
+		console.log("\tThe Service file was created.");
+
+		console.log("");
+
+		//
+		//	Tell the user what to do next
+		//
+		console.log("\t1. sudo cp " + parsed.name + ".service /etc/systemd/system");
+		console.log("\t2. sudo systemctl daemon-reload");
+		console.log("\t3. sudo systemctl start " + parsed.name);
+		console.log("\t4. sudo systemctl enable " + parsed.name);
+
+		console.log("");
+	});
+
+});
+
+
+
