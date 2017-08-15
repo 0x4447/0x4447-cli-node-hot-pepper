cbc2bfd5051777c23ba0fca184b33c61e683b235
David Gatti <github@gatti.pl>
2017-08-15 08:47:32 +0200

    Improved the flow

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ let os = require('os');
 let exec = require('child_process').exec;
 
 //
+//	1. 	Create a container that will hold all the data for the chained
+//		promises.
 //
 let container = {};
 
 //
-//
+//	2.	Since we need root privileges, we need to check for that
 //
 check_if_we_are_root(container)
 	.then(function(container){
@@ -22,14 +24,35 @@ check_if_we_are_root(container)
 	}).then(function(container){
 
 		//
-		//	1.	Generate the file
+		//	1.	Make sure the package.json is present before we try to load it
 		//
-		return read_necessary_data(container);
+		return check_if_package_is_present(container);
 
 	}).then(function(container){
 
 		//
-		//	1.	Generate the file
+		//	1.	Gather all the necessary data from the project
+		//
+		return read_necessary_data(container);
+
+	})..then(function(container){
+
+		//
+		//	1.	Gather all the necessary data from the project
+		//
+		return check_if_something_is_missing(container);
+
+	}).then(function(container){
+
+		//
+		//	1.	Gather all the necessary data from the project
+		//
+		return get_os_settings(container);
+
+	}).then(function(container){
+
+		//
+		//	1.	Generate the config file
 		//
 		return create_the_service_file(container);
 
@@ -43,21 +66,21 @@ check_if_we_are_root(container)
 	}).then(function(container){
 
 		//
-		//	1.	Restart systemD
+		//	1.	Tell SystemD to reload all the service files
 		//
 		return reaload_systemd_daemon(container);
 
 	}).then(function(container){
 
 		//
-		//	1.	Restart systemD
+		//	1.	Start the new service
 		//
 		return start_the_new_service(container);
 
 	}).then(function(container){
 
 		//
-		//	1.	Restart systemD
+		//	1.	Tell SystemD to start the new service at boot
 		//
 		return enable_autostart(container);
 
@@ -67,8 +90,7 @@ check_if_we_are_root(container)
 		//	1.	Let the user know all went well
 		//
 		console.log("\n");
-		console.log("\n");
-		console.log("\tService Added.");
+		console.log("\tDone!");
 		console.log("\n");
 		console.log("\n");
 
@@ -107,8 +129,10 @@ check_if_we_are_root(container)
 //
 //	We need root to be able to:
 //
-//	- save the file in to the Nginx directory
-//	- Restart Nginx
+//	- save the file in to the systemD directory
+//	- Restart systemD
+//	- Start the new service
+//	- Mark it as bootable
 //
 function check_if_we_are_root(container)
 {
@@ -137,7 +161,7 @@ function check_if_we_are_root(container)
 
 //
 //	Make sure the systemD folder exists and is preset so we can save our
-//	config file
+//	config file.
 //
 function check_if_systemd_is_present(container)
 {
@@ -146,12 +170,12 @@ function check_if_systemd_is_present(container)
 		//
 		//	1.	Check if the SystemD directory exists
 		//
-		let is_systemd = fs.existsSync("/etc/systemd/system");
+		let is_present = fs.existsSync("/etc/systemd/system");
 
 		//
 		//	2.	Warn the user that the directory is not present
 		//
-		if(!is_systemd)
+		if(!is_present)
 		{
 			return reject(new Error("SystemD directory not found"));
 		}
@@ -165,7 +189,36 @@ function check_if_systemd_is_present(container)
 }
 
 //
-//	After we have all the data we can create the config file for the site.
+//	Make sure package.json file exists
+//
+function check_if_package_is_present(container)
+{
+	return new Promise(function(resolve, reject) {
+
+		//
+		//	1.	Check if the package.json file exists
+		//
+		let is_present = fs.existsSync("package.json");
+
+		//
+		//	2.	Warn the user that the package.json is missing
+		//
+		if(!is_present)
+		{
+			return reject(new Error("package.json is missing"));
+		}
+
+		//
+		//	-> Move to the next chain
+		//
+		return resolve(container);
+
+	});
+}
+
+//
+//	Get all the necessary data from the project folder so we can create
+//	the correct config file.
 //
 function read_necessary_data(container)
 {
@@ -189,13 +242,30 @@ function read_necessary_data(container)
 			//
 			let parsed = JSON.parse(data);
 
+			let description = "";
+			let documentation = "";
+
 			//
-			//	3.	Get all the necessary data
+			//	3.	Make sure the name is present
 			//
-			let description = parsed.name;
-			let documentation = parsed.repository.url;
-			let cwd = process.cwd();
-			let user = os.userInfo().username;
+			if(parsed.name)
+			{
+				description = parsed.name;
+			}
+
+			//
+			//	4.	Make sure the repo is present
+			//
+			if(parsed.repository)
+			{
+				//
+				//	1.	Make sure the URL repo is present also
+				//
+				if(parsed.repository.url)
+				{
+					documentation = parsed.repository.url;
+				}
+			}
 
 			//
 			//	4.	Save the file in to memory
@@ -203,9 +273,7 @@ function read_necessary_data(container)
 			container.service_data = {
 				name: description,
 				description,
-				documentation,
-				cwd,
-				user
+				documentation
 			}
 
 			//
@@ -218,7 +286,71 @@ function read_necessary_data(container)
 }
 
 //
-//	After we have all the data we can create the config file for the site.
+//	Make sure package.json file exists
+//
+function check_if_something_is_missing(container)
+{
+	return new Promise(function(resolve, reject) {
+
+		let errors = [];
+
+		if(!container.service_data.name)
+		{
+			errors.push(new Error("Name is missing"));
+		}
+
+		if(!container.service_data.description)
+		{
+			errors.push(new Error("Description is missing"));
+		}
+
+		if(!container.service_data.documentation)
+		{
+			errors.push(new Error("Repo URL is missing"));
+		}
+
+		if(errors.length > 0)
+		{
+			let error_message = errors.join(" ");
+
+			return reject(new Error(error_message));
+		}
+
+		//
+		//	-> Move to the next chain
+		//
+		return resolve(container);
+
+	});
+}
+
+//
+//	Make sure package.json file exists
+//
+function get_os_settings(container)
+{
+	return new Promise(function(resolve, reject) {
+
+		//
+		//	1. Get the path to the project
+		//
+		container.service_data.cwd = process.cwd();
+
+		//
+		//	2.	Get the user name of the loged in user
+		//
+		container.service_data.user = os.userInfo().username;
+
+		//
+		//	-> Move to the next chain
+		//
+		return resolve(container);
+
+	});
+}
+
+//
+//	After we have all the data we can create the config file for the service.
 //
 function create_the_service_file(container)
 {
@@ -308,13 +440,6 @@ function save_the_file(container)
 			}
 
 			//
-			//	Show the user what happened
-			//
-			console.log("");
-			console.log("\tService Saved");
-			console.log("");
-
-			//
 			//	-> Move to the next chain
 			//
 			return resolve(container);
@@ -325,7 +450,7 @@ function save_the_file(container)
 }
 
 //
-//	Restart Nginx so we can start using our site.
+//	Tell SystemD to reload and get any new service file
 //
 function reaload_systemd_daemon(container)
 {
@@ -360,7 +485,7 @@ function reaload_systemd_daemon(container)
 }
 
 //
-//	Restart Nginx so we can start using our site.
+//	Start the newly created site
 //
 function start_the_new_service(container)
 {
@@ -395,7 +520,7 @@ function start_the_new_service(container)
 }
 
 //
-//	Restart Nginx so we can start using our site.
+//	Tell SystemD to start our server every time the system boots
 //
 function enable_autostart(container)
 {
